ac068610781dc7aec30fc124ff4abe9caff3868c
Matt Eland <Matt.Eland@GMail.com>
2019-10-27 01:00:33 -0400

    Converted to TypeScript

--- Logic.js
+++ Logic.js
@@ -1,114 +1,115 @@
-var testCases = [];
-var nextTestCaseId = 2;
-
-class TestCase {
-    constructor(testCaseName, id) {
+"use strict";
+var TestCase = /** @class */ (function () {
+    function TestCase(testCaseName, id) {
         this.name = testCaseName;
         this.isPassing = true;
         this.id = id;
     }
-}
-
-function buildInitialData() {
-    return [{
-        name: 'The app should not crash on startup',
-        isPassing: true,
-        id: '1'
-    }];
-}
-
+    return TestCase;
+}());
 function addTestCase() {
-    const textBox = document.getElementById('txtTestName');
-    const testCaseName = textBox.value;
-
+    var textBox = document.getElementById('txtTestName');
+    var testCaseName = textBox.value;
     if (testCaseName) {
-        const testCase = new TestCase(testCaseName, nextTestCaseId++);
-        testCases.push(testCase);
-
-        updateTestCases();
-
+        testManager.addTestCase(testCaseName);
         textBox.value = '';
-    } else {
+    }
+    else {
         alert('You must enter a test case name');
     }
 }
-
-function updateTestCases() {
+var TestManagerApp = /** @class */ (function () {
+    function TestManagerApp() {
+        this.testCases = [];
+        this.nextTestCaseId = 2;
+        this.testCases = TestManagerApp.buildInitialData();
+    }
+    TestManagerApp.buildInitialData = function () {
+        return [{
+                name: 'The app should not crash on startup',
+                isPassing: true,
+                id: 1
+            }];
+    };
+    TestManagerApp.prototype.findTestCaseById = function (id) {
+        for (var _i = 0, _a = this.testCases; _i < _a.length; _i++) {
+            var testCase = _a[_i];
+            if (testCase.id === id)
+                return testCase;
+        }
+        return null;
+    };
+    TestManagerApp.prototype.deleteTestCase = function (testCase) {
+        var index = this.testCases.indexOf(testCase);
+        this.testCases.splice(index, 1);
+    };
+    TestManagerApp.prototype.addTestCase = function (testCaseName) {
+        var testCase = new TestCase(testCaseName, this.nextTestCaseId++);
+        this.testCases.push(testCase);
+        updateTestCases(this);
+    };
+    return TestManagerApp;
+}());
+var testManager = new TestManagerApp();
+function updateTestCases(app) {
     // Clear the existing items
     var list = document.getElementById('listTestCases');
     list.innerHTML = '';
-
-    for (testcase of this.testCases) {
+    for (var _i = 0, _a = app.testCases; _i < _a.length; _i++) {
+        var testcase = _a[_i];
         addTestCaseToUI(testcase);
     }
-
     // Show or hide the empty items list
-    if (this.testCases.length > 0) {
+    if (app.testCases.length > 0) {
         hideAddItemsPrompt();
-    } else {
+    }
+    else {
         showAddItemsPrompt();
     }
 }
-
 function addTestCaseToUI(testCase) {
     var list = document.getElementById('listTestCases');
-
     var child = document.createElement('div');
     child.className = 'list-group-item';
-
     var header = document.createElement('h3');
     header.innerText = testCase.name;
     header.className = testCase.isPassing ? 'text-success' : 'text-danger';
     child.appendChild(header);
-
     var buttons = document.createElement('div');
     buttons.className = 'btn-group';
-    buttons.innerHTML = `<button class='btn btn-success' onclick='passTestCase(${testCase.id});'>Pass</button>
-                         <button class='btn btn-danger' onclick='failTestCase(${testCase.id});'>Fail</button>
-                         <button class='btn btn-outline-danger ml-2' onclick='deleteTestCase(${testCase.id});'>Delete</button>`;
+    buttons.innerHTML = "<button class='btn btn-success' onclick='passTestCase(" + testCase.id + ");'>Pass</button>\n                         <button class='btn btn-danger' onclick='failTestCase(" + testCase.id + ");'>Fail</button>\n                         <button class='btn btn-outline-danger ml-2' onclick='deleteTestCase(" + testCase.id + ");'>Delete</button>";
     child.appendChild(buttons);
-
     list.appendChild(child);
 }
-
 function passTestCase(id) {
-    const testcase = findTestCaseById(id);
-    testcase.isPassing = true;
-    updateTestCases();
+    var testcase = testManager.findTestCaseById(id);
+    if (testcase) {
+        testcase.isPassing = true;
+    }
+    updateTestCases(testManager);
 }
-
 function failTestCase(id) {
-    const testCase = findTestCaseById(id);
-    testcase.isPassing = false;
-    updateTestCases();
+    var testcase = testManager.findTestCaseById(id);
+    if (testcase) {
+        testcase.isPassing = false;
+    }
+    updateTestCases(testManager);
 }
-
 function deleteTestCase(id) {
-    const testcase = findTestCaseById(id);
-    const index = this.testCases.indexOf(testcase);
-    this.testCases.splice(index, 1);
-    updateTestCases();
+    var testcase = testManager.findTestCaseById(id);
+    if (testcase) {
+        testManager.deleteTestCase(testcase);
+    }
+    updateTestCases(testManager);
 }
-
 function hideAddItemsPrompt() {
-    const label = document.getElementById('lblNoTestCases');
+    var label = document.getElementById('lblNoTestCases');
     label.className = 'invisible';
 }
-
-function showAddItemPrompt() {
-    const label = document.getElementById('lblNoTestCases');
+function showAddItemsPrompt() {
+    var label = document.getElementById('lblNoTestCases');
     label.className = 'text-muted';
 }
-
-function findTestCaseById(id) {
-    for (testcase of this.testCases) {
-        if (testcase.id === id) return testcase;
-    }
-
-    return null;
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-    testCases = buildInitialData();
-    updateTestCases();
+document.addEventListener("DOMContentLoaded", function () {
+    updateTestCases(testManager);
 });
---
+++ Logic.ts
@@ -0,0 +1,139 @@
+class TestCase {
+    constructor(testCaseName: string, id: number) {
+        this.name = testCaseName;
+        this.isPassing = true;
+        this.id = id;
+    }
+
+    public name: string;
+    public isPassing: boolean;
+    public id: number;
+}
+
+function addTestCase() {
+    const textBox = <HTMLInputElement>document.getElementById('txtTestName');
+    const testCaseName = textBox.value;
+
+    if (testCaseName) {
+        testManager.addTestCase(testCaseName);
+
+        textBox.value = '';
+    } else {
+        alert('You must enter a test case name');
+    }
+}
+
+class TestManagerApp {
+    public testCases: TestCase[] = [];
+    public nextTestCaseId: number = 2;
+
+    constructor() {
+        this.testCases = TestManagerApp.buildInitialData();
+    }
+
+    private static buildInitialData(): TestCase[] {
+        return [{
+            name: 'The app should not crash on startup',
+            isPassing: true,
+            id: 1
+        }];
+    }
+
+    public findTestCaseById(id: number) {
+        for (const testCase of this.testCases) {
+            if (testCase.id === id) return testCase;
+        }
+
+        return null;
+    }
+
+    public deleteTestCase(testCase: TestCase) {
+        const index = this.testCases.indexOf(testCase);
+        this.testCases.splice(index, 1);
+    }
+
+    addTestCase(testCaseName: string) {
+        const testCase = new TestCase(testCaseName, this.nextTestCaseId++);
+        this.testCases.push(testCase);
+
+        updateTestCases(this);
+    }
+}
+
+const testManager: TestManagerApp = new TestManagerApp();
+
+function updateTestCases(app: TestManagerApp) {
+    // Clear the existing items
+    const list = <HTMLElement>document.getElementById('listTestCases');
+    list.innerHTML = '';
+
+    for (const testcase of app.testCases) {
+        addTestCaseToUI(testcase);
+    }
+
+    // Show or hide the empty items list
+    if (app.testCases.length > 0) {
+        hideAddItemsPrompt();
+    } else {
+        showAddItemsPrompt();
+    }
+}
+
+function addTestCaseToUI(testCase: TestCase) {
+    const list = <HTMLElement>document.getElementById('listTestCases');
+
+    const child = document.createElement('div');
+    child.className = 'list-group-item';
+
+    const header = document.createElement('h3');
+    header.innerText = testCase.name;
+    header.className = testCase.isPassing ? 'text-success' : 'text-danger';
+    child.appendChild(header);
+
+    const buttons = document.createElement('div');
+    buttons.className = 'btn-group';
+    buttons.innerHTML = `<button class='btn btn-success' onclick='passTestCase(${testCase.id});'>Pass</button>
+                         <button class='btn btn-danger' onclick='failTestCase(${testCase.id});'>Fail</button>
+                         <button class='btn btn-outline-danger ml-2' onclick='deleteTestCase(${testCase.id});'>Delete</button>`;
+    child.appendChild(buttons);
+
+    list.appendChild(child);
+}
+
+function passTestCase(id: number) {
+    const testcase = testManager.findTestCaseById(id);
+    if (testcase) {
+        testcase.isPassing = true;
+    }
+    updateTestCases(testManager);
+}
+
+function failTestCase(id: number) {
+    const testcase = testManager.findTestCaseById(id);
+    if (testcase) {
+        testcase.isPassing = false;
+    }
+    updateTestCases(testManager);
+}
+
+function deleteTestCase(id: number) {
+    const testcase = testManager.findTestCaseById(id);
+    if (testcase) {
+        testManager.deleteTestCase(testcase);
+    }
+    updateTestCases(testManager);
+}
+
+function hideAddItemsPrompt() {
+    const label = <HTMLElement> document.getElementById('lblNoTestCases');
+    label.className = 'invisible';
+}
+
+function showAddItemsPrompt() {
+    const label = <HTMLElement>document.getElementById('lblNoTestCases');
+    label.className = 'text-muted';
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    updateTestCases(testManager);
+});
